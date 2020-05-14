c2f5a1871c8c01161ed30da445e573c1d6f2e74d
Jobin Jose <jbntestmail@gmail.com>
2020-05-14 13:44:56 -0500

    First commit

---
+++ CoolJob/CoolJob/CoolJob.h
@@ -0,0 +1,19 @@
+//
+//  CoolJob.h
+//  CoolJob
+//
+//  Created by Jobin Jose on 14/05/20.
+//  Copyright © 2020 Jobin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoolJob.
+FOUNDATION_EXPORT double CoolJobVersionNumber;
+
+//! Project version string for CoolJob.
+FOUNDATION_EXPORT const unsigned char CoolJobVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoolJob/PublicHeader.h>
+
+
