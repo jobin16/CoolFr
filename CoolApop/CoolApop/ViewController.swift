39da1d2424df9277a734ce636b925d757b59d035
Jobin Jose <jbntestmail@gmail.com>
2020-05-16 15:45:38 -0500

    [Author][JIRA-MPU-0000] : Description

---
+++ CoolApop/CoolApop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoolApop
+//
+//  Created by Jobin Jose on 16/05/20.
+//  Copyright © 2020 Jobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
